dfeeb4b57d2a3aeb4cedb8031675f8e28fbaf293
TruffleBug <lisa.ho1022@gmail.com>
2024-08-28 14:49:12 -0600

    Complete project
    Completed project. Includes bonus of darkening the tile with each pass.

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
 const gridContainer = document.querySelector('#gridContainer');
 
-let boxesOnSide = 4;
-let boxDim = 920 / boxesOnSide;
+const newGridButton = document.querySelector('button');
 
-for (let i = 0; i < boxesOnSide**2; i++) {
-    let box = document.createElement('div');
-    box.setAttribute('class', 'box');
-    gridContainer.appendChild(box);
+let boxesOnSide = 16;
 
-    box.addEventListener('mouseover', (event) => {
-        event.target.style.backgroundColor = 'lightgreen';
-    })
-
-    box.style.width = `${boxDim}px`;
-    box.style.height = `${boxDim}px`;
+function enterBoxNum() {
+    boxesOnSide = Math.round(prompt('Enter number of boxes per side', '1-100'));
+    if (boxesOnSide > 100) {
+        alert('That\'s too damn high');
+        enterBoxNum();
+    } else if (boxesOnSide < 1) {
+        alert('Enter value above 0');
+        enterBoxNum();
+    } else if (boxesOnSide > 0 && boxesOnSide < 101) {
+        gridContainer.textContent = '';
+        newGrid(boxesOnSide)
+    } else {
+        alert('Not a number') 
+        enterBoxNum()
+    }
 }
+
+function newGrid(boxesOnSide) {
+    for (let i = 0; i < boxesOnSide**2; i++) {
+        let box = document.createElement('div');
+        box.setAttribute('class', 'box');
+        gridContainer.appendChild(box);
+                
+        boxDim = 920 / boxesOnSide;
+        box.style.width = `${boxDim}px`;
+        box.style.height = `${boxDim}px`;
+        
+        box.addEventListener('mouseover', (event) => {
+            event.target.style.backgroundColor = 'lightgreen';
+        })
+
+        box.addEventListener('mouseover', () => {
+            event.target.style.filter += 'brightness(80%)';
+        })
+    }
+}
+
+function randomRGB() {
+    let r = randomInteger
+}
+
+newGridButton.addEventListener('click', enterBoxNum)
+
+window.addEventListener('load', newGrid(boxesOnSide))
